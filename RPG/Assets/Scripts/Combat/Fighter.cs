7093438926c67da0bea85ceabe59220065354ec7
Richard <richardjalba@gmail.com>
2021-02-11 10:38:31 -0800

    Attack Animation and Basic Damage Scripts

--- RPG/Assets/Scripts/Combat/Fighter.cs
+++ RPG/Assets/Scripts/Combat/Fighter.cs
@@ -7,11 +7,16 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
 
         Transform target; 
+        float timeSinceLastAttack = 0;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
 
             if (!GetIsInRange())
@@ -21,7 +26,25 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehavior();
             }
+        }
+
+        private void AttackBehavior()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                // Triggers Hit() event
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
+        }
+
+        // Animation Event
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
 
         private bool GetIsInRange()
@@ -39,5 +62,7 @@ namespace RPG.Combat
         {
             target = null;
         }
+
+        
     }
 }
