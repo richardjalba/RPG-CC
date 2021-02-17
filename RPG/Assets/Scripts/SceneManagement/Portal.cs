604c6be71ad5dec789ac985a92fa8489371cd649
Richard <richardjalba@gmail.com>
2021-02-17 09:24:29 -0800

    Adding Saving Asset Pack

--- RPG/Assets/Scripts/SceneManagement/Portal.cs
+++ RPG/Assets/Scripts/SceneManagement/Portal.cs
@@ -7,14 +7,8 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
-        enum DestinationIdentifier
-        {
-            A, B, C, D, E
-        }
-
         [SerializeField] int sceneToLoad = -1;
         [SerializeField] Transform spawnPoint;
-        [SerializeField] DestinationIdentifier destination;
 
         private void OnTriggerEnter(Collider other) {
             if (other.tag == "Player")
@@ -25,12 +19,6 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
-            if (sceneToLoad < 0)
-            {
-                Debug.LogError("Scene to load not set.");
-                yield break;
-            }
-
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
             
@@ -52,7 +40,6 @@ namespace RPG.SceneManagement
             foreach (Portal portal in FindObjectsOfType<Portal>())
             {
                 if (portal == this) continue;
-                if (portal.destination != destination) continue;
 
                 return portal;
             }
