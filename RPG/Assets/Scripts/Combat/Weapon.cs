2c934fdd1ac3f93e33375d30cb3b482e484fb058
Richard <richardjalba@gmail.com>
2021-02-18 17:20:35 -0800

    Additional Weapons, Bugs introduced; Skipping

--- RPG/Assets/Scripts/Combat/Weapon.cs
+++ RPG/Assets/Scripts/Combat/Weapon.cs
@@ -2,32 +2,38 @@
 
 namespace RPG.Combat
 {
-    [CreateAssetMenu(fileName = "Weapon", menuName = "RPG/Weapon", order = 0)]
-public class Weapon : ScriptableObject 
-{
-    [SerializeField] AnimatorOverrideController animatorOverride = null;
-    [SerializeField] GameObject equippedPrefab = null;
-    [SerializeField] float weaponDamage = 5f;
-    [SerializeField] float weaponRange = 2f;
+    [CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/Make New Weapon", order = 0)]
+    public class Weapon : ScriptableObject
+    {
+        [SerializeField] AnimatorOverrideController animatorOverride = null;
+        [SerializeField] GameObject equippedPrefab = null;
+        [SerializeField] float weaponDamage = 5f;
+        [SerializeField] float weaponRange = 2f;
+        [SerializeField] bool isRightHanded = true;
 
-    public void Spawn(Transform handTransform, Animator animator)
-    {
-        if (equippedPrefab != null) {
-            Instantiate(equippedPrefab, handTransform);
+        public void Spawn(Transform rightHand, Transform leftHand, Animator animator)
+        {
+            if (equippedPrefab != null)
+            {
+                Transform handTransform;
+                if (isRightHanded) handTransform = rightHand;
+                else handTransform = leftHand;
+                Instantiate(equippedPrefab, handTransform);
+            }
+            if (animatorOverride != null)
+            {
+                animator.runtimeAnimatorController = animatorOverride; 
+            }
         }
-        if(animatorOverride != null) {
-            animator.runtimeAnimatorController = animatorOverride;
-        }
-    }
 
-    public float GetDamage()
-    {
-        return weaponDamage;
+        public float GetDamage()
+        {
+            return weaponDamage;
+        }
+
+        public float GetRange()
+        {
+            return weaponRange;
+        }
     }
-    
-    public float GetRange()
-    {
-        return weaponRange;
-    }
-}
 }
